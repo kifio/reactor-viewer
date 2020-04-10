f9ec1b1e55bcca4acf68853ad665976aff29d373
kifio <kg.of.morphine@gmail.com>
2020-04-10 21:47:02 +0200

    kotlin setup

---
+++ reactor-parser/src/commonMain/kotlin/Reactor.kt
@@ -0,0 +1,19 @@
+/**
+ * Публичный апи парсера.
+ */
+class Reactor {
+
+    fun getLastPage(tag: String): Pair<Int, List<Post>> {
+        return Pair(0, emptyList())
+    }
+
+    fun getPage(page: Int): List<Post>{
+        return emptyList()
+    }
+
+    /**
+     * @param imageUrl - url картинки в посте
+     * @param tag - tag по которому делался поиск
+     */
+    data class Post(val imageUrl: String, val tag: String)
+}
